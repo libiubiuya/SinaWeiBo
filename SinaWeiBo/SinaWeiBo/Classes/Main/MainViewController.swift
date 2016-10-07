3c9594482a9e20f5f7a8472c296ee9d944ef7b51
libiubiuya <18366107704@163.com>
2016-10-07 21:56:07 +0800

    1.搭建项目架构

---
+++ SinaWeiBo/SinaWeiBo/Classes/Main/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  SinaWeiBo
+//
+//  Created by 李好一 on 16/10/7.
+//  Copyright © 2016年 李好一. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
